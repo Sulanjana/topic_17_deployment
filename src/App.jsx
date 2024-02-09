eed1f9bff4fbf1ab1c1446babf7af7c1bfe97341
Reza <eza.purnama.rp@gmail.com>
2024-02-09 11:43:38 +0700

    topics-14

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <div className="text">Selamat Datang Di Website Reza</div>
+        <p>
+          Saya menggunakan netlify untuk proses deployment, dengan netlify hanya
+          butuh beberapa menit saja untuk melakukan proses deployment, sangat
+          mudah, sangat cepat dan tidak sulit untuk di pelajari
+        </p>
+      </div>
+    </>
+  );
+}
+
+export default App;
